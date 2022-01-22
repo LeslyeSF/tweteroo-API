fdf1cca20688c75996656f69869b083381be031e
Leslye Soares Ferreira <leslyesoares@gmail.com>
2022-01-22 18:43:19 -0300

    feat: route tweets

--- index.js
+++ index.js
@@ -3,6 +3,63 @@ import cors from "cors";
 
 const server = express();
 let userLogin =[];
+let tweets = [
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	},
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+	}
+];
 
 server.use(cors());
 server.use(express.json());
@@ -13,5 +70,27 @@ server.post("/sign-up", (req,res)=>{
   console.log("Login ok");
 });
 
+server.get("/tweets",(req,res)=>{
+  
+  let sendTweets = tweets.filter((data, index)=>{
+    if(index > (tweets.length - 11)){
+      return true;
+    }
+    return false;
+  });
+  res.send(sendTweets.reverse());
+
+});
+
+server.post("/tweets", (req, res)=>{
+  const tweet = {
+    username: req.body.username,
+    avatar: userLogin[userLogin.length-1].avatar,
+    tweet: req.body.tweet
+  };
+  
+  tweets.push(tweet);
+  res.send("ok");
+});
 
 server.listen(5000);
