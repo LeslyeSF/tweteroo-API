5c86fd35a5aca5a219e270855f067bbe63374607
Leslye Soares Ferreira <leslyesoares@gmail.com>
2022-01-23 22:22:29 -0300

    feat: route  Username

--- index.js
+++ index.js
@@ -60,14 +60,14 @@ let tweets = [
 	  tweet: "eu amo o hub"
 	}
 ];
+let tweetsUser = [];
 
 server.use(cors());
 server.use(express.json());
 
 server.post("/sign-up", (req,res)=>{
   userLogin.push(req.body);
-  res.send("ok");
-  console.log("Login ok");
+	res.status(201).send("ok");
 });
 
 server.get("/tweets",(req,res)=>{
@@ -81,16 +81,29 @@ server.get("/tweets",(req,res)=>{
   res.send(sendTweets.reverse());
 
 });
+server.get("/tweets/:USERNAME",(req,res)=>{
+	const username = req.params.USERNAME;
+  
+  let sendTweets = tweets.filter((data, index)=>{
+    if(username === data.username){
+      return true;
+    }
+    return false;
+  });
+  res.send(sendTweets.reverse());
+
+});
 
 server.post("/tweets", (req, res)=>{
   const tweet = {
-    username: req.body.username,
+    username: req.headers.user,
     avatar: userLogin[userLogin.length-1].avatar,
     tweet: req.body.tweet
   };
   
   tweets.push(tweet);
-  res.send("ok");
+  tweetsUser.push(tweet);
+	res.status(201).send("ok");
 });
 
 server.listen(5000);
